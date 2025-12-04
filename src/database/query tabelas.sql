c96585c7c9350389c78b0e6e3a0437cf07377463
D-Foschini <daniel.foschini@sptech.school>
2025-12-03 23:35:34 -0300

    Fixes de minor Bugs & Preparação para Host em servidor

--- src/database/query tabelas.sql
+++ src/database/query tabelas.sql
@@ -1,25 +1,28 @@
 CREATE DATABASE shakai;
+
 USE shakai;
 
-CREATE TABLE cadastro(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    nick VARCHAR(45),
+CREATE TABLE cadastro (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nick VARCHAR(45) UNIQUE,
     email VARCHAR(45) UNIQUE,
     passwrd VARCHAR(45)
 );
 
-CREATE TABLE veiculo(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE veiculo (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     fkCadastro INT,
-    CONSTRAINT fk_cadastro_veiculo FOREIGN KEY (fkCadastro) REFERENCES cadastro(id),
+    CONSTRAINT fk_cadastro_veiculo FOREIGN KEY (fkCadastro) REFERENCES cadastro (id),
     modelo VARCHAR(45),
     descricao VARCHAR(420)
 );
 
-CREATE TABLE informa(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+select * from veiculo;
+
+CREATE TABLE informa (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     fkVeiculo INT,
-    CONSTRAINT fk_veiculo_informa FOREIGN KEY (fkVeiculo) REFERENCES veiculo(id),
+    CONSTRAINT fk_veiculo_informa FOREIGN KEY (fkVeiculo) REFERENCES veiculo (id),
     mass INT,
     bar INT,
     country VARCHAR(45),
@@ -33,20 +36,38 @@ CREATE TABLE informa(
     accelerate INT
 );
 
-CREATE TABLE grafico(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE grafico (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     fkVeiculo INT,
-    CONSTRAINT fk_veiculo_grafico FOREIGN KEY (fkVeiculo) REFERENCES veiculo(id),
-    rpmIndex INT,
-	horsePower INT,
-    torque INT
+    CONSTRAINT fk_veiculo_grafico FOREIGN KEY (fkVeiculo) REFERENCES veiculo (id),
+    hp_A INT,
+    hp_B INT,
+    hp_C INT,
+    hp_D INT,
+    hp_E INT,
+    hp_F INT,
+    hp_G INT,
+    hp_H INT,
+    hp_I INT,
+    hp_J INT,
+    hp_K INT,
+    hp_L INT,
+    hp_M INT,
+    hp_N INT,
+    hp_O INT,
+    trq_A INT,
+    trq_B INT,
+    trq_C INT,
+    trq_D INT,
+    trq_E INT,
+    trq_F INT,
+    trq_G INT,
+    trq_H INT,
+    trq_I INT,
+    trq_J INT,
+    trq_K INT,
+    trq_L INT,
+    trq_M INT,
+    trq_N INT,
+    trq_O INT
 );
-
-
-
-use shakai;
-
-select * from cadastro;
-delete from cadastro where id >0;
-alter table cadastro AUTO_INCREMENT = 0;
-select * from veiculo;
